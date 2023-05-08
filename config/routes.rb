e0351830eb149a214ff18242688d2b619a381318
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2023-05-08 09:40:52 -0300

    up to 9.2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
 end
